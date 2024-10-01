280a931edd021f54225f9e3b706264b0e42942ad
Justine Okumu <okumujustine01@gmail.com>
2024-10-01 10:46:51 -0500

    life schedule initial application

---
+++ src/main/java/com/wanlife/wanlife/WanlifeApplication.java
@@ -0,0 +1,13 @@
+package com.wanlife.wanlife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WanlifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WanlifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wanlife/wanlife/WanlifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanlife.wanlife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WanlifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
